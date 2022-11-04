b253129d0d8ac379a79093ee5ab31fcdfa06869e
Anders Wallin <anders.e.e.wallin@gmail.com>
2022-11-04 19:16:24 +0200

    make mathjax work

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,8 @@ import pylion
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode',
               'sphinx.ext.mathjax']
 
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
